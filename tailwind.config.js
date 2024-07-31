c5dc3999143b1e22993ec4182831a74277e06fb2
lucy <eg.piolo1@gmail.com>
2024-07-31 21:36:06 +0800

    I feel like shit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
   content: [
-    "./src/**/*.html",
-    "./src/**/*.js",
+    "./public/*.html",
+    "./src/**/*.js",      // Adjusted to capture JS files in src if any
+    "./src/**/*.css",     // Ensure it captures CSS files in src
   ],
   theme: {
     extend: {},
